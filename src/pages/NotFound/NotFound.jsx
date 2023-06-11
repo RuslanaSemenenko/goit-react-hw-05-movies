4c992dccb9279a4b7d3a37ecd9c3bfa23dd55b44
Ruslana Semenenko <katotochkaaa@gmail.com>
2023-06-11 18:38:46 +0100

    homework

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,55 @@
-import { NotFoundBlock, NotFoundText, NotFoundLink } from '../Pages.styled';
+import { fetchTrendingApi } from '../../api/fetchApi';
+import { useState, useEffect } from 'react';
+import Notiflix from 'notiflix';
+
+import {
+  MoviesList,
+  LinkItem,
+  MovieTitle,
+  TrendingTitle,
+} from '../Pages.styled';
 
 export const NotFound = () => {
+  const [trendings, setTrendings] = useState([]);
+
+  useEffect(() => {
+    async function moviesFetch() {
+      try {
+        const { results } = await fetchTrendingApi();
+        if (results < 1) {
+          Notiflix.Notify.warning("We can't find it, try again");
+        }
+        setTrendings(results);
+      } catch {
+        Notiflix.Notify.warning('Something wrong, try again please');
+      }
+    }
+    moviesFetch();
+  }, []);
+
   return (
-    <NotFoundBlock>
-      <NotFoundText>Sorry, something wrong</NotFoundText>
-      <NotFoundLink to="/">Back to main page</NotFoundLink>
-    </NotFoundBlock>
+    <>
+      <TrendingTitle>Trending movies</TrendingTitle>
+      {trendings && (
+        <MoviesList>
+          {trendings.map(movie => {
+            let posterPath;
+            if (movie.poster_path) {
+              posterPath = `https://image.tmdb.org/t/p/w400/${movie.poster_path}`;
+            } else {
+              posterPath =
+                'https://i.pinimg.com/736x/64/71/62/6471622474d071e6d5b6c111a9e18a5d.jpg';
+            }
+
+            return (
+              <LinkItem key={movie.id} to={`/movies/${movie.id}`}>
+                <img src={posterPath} width="400" alt={movie.title} />
+                <MovieTitle>{movie.title}</MovieTitle>
+              </LinkItem>
+            );
+          })}
+        </MoviesList>
+      )}
+    </>
   );
 };
